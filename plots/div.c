7796d2c5126f3f5306322398813347ab499ebe63
isaac098098 <joseluis-jl-1@hotmail.com>
2025-05-31 20:31:55 -0600

    final version

--- plots/div.c
+++ plots/div.c
@@ -6,6 +6,24 @@ int mcd(int a, int b) {
     return b == 0 ? a > 0 ? a : -a : mcd(b, a % b);
 }
 
+double u(int n) {
+    if(n >= 0)
+        return 1.0;
+    else
+        return 0;
+}
+
+double ex(double amp, double n) {
+    return pow(amp, -n);
+}
+
+double perex(int n, int p, double amp) {
+    if(n < 0)
+        return ex(amp, -n % p);
+    else
+        return ex(amp, n % p);
+}
+
 int dsum(int n) {
     int sum=0;
     for(int i=1; i<=n; i++)
@@ -46,11 +64,13 @@ int ram(int r, int n) {
 }
 
 int main(int argc, char** argv) {
-    FILE* div=fopen("div.csv", "w");
+    FILE* div=fopen("plot8.csv", "w");
     /*for(int j=-10; j <= atoi(argv[1]); j++)*/
     /*    fprintf(div, "%d, %d\n", j, mcd(3,j));*/
-    for(int j= atoi(argv[1]); j <= atoi(argv[2]); j++)
-        fprintf(div, "%d, %1.5f\n", j, 2.0*ram(1,j) - 2.0*ram(2,j) - 1.0*ram(3,j) + 1.0*ram(6,j));
+    for(int j= atoi(argv[1]); j < 0; j++)
+        fprintf(div, "%d, %1.5f\n", j, perex(j+3*40, 40, 1.1));
+    for(int j=0; j <= atoi(argv[2]); j++)
+        fprintf(div, "%d, %1.5f\n", j, perex(j, 40, 1.1));
     fclose(div);
     /*printf("%d\n", ram(atoi(argv[1]), atoi(argv[2])));*/
     return 0;
